97a36f293fbdd63d9691758fc5a070b1c3a89b2a
yad <yad@yad.com>
2023-11-19 09:26:16 +0100

    ci: add simple update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+
+git pull
+
+sudo dotnet publish --runtime linux-arm --self-contained -o /opt/comicsbox
+
+sudo chown -R comicsbox:comicsbox /opt/comicsbox/
+
+sudo systemctl restart comicsbox.service
+
+sudo systemctl status comicsbox.service
