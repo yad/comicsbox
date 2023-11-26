a6bf220cbad171a3ffba2a3554b4cf33d1a2211b
yad <yad@yad.com>
2023-11-26 20:58:55 +0100

    feat: add purge extract pdf

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ builder.Services.AddTransient<PdfReaderService, PdfReaderService>();
 
 builder.Services.AddSingleton<BookInfoService, BookInfoService>();
 builder.Services.AddSingleton<ThumbnailProvider, ThumbnailProvider>();
+builder.Services.AddSingleton<ReaderCleanerWorkerService, ReaderCleanerWorkerService>();
 builder.Services.AddSingleton<ThumbnailWorkerService, ThumbnailWorkerService>();
 builder.Services.AddSingleton<ZipCleanerWorkerService, ZipCleanerWorkerService>();
 
+builder.Services.AddHostedService<ReaderCleanerWorkerService>();
 builder.Services.AddHostedService<ThumbnailWorkerService>();
 builder.Services.AddHostedService<ZipCleanerWorkerService>();
 
---
+++ Services/Worker/ReaderCleanerWorkerService.cs
@@ -0,0 +1,83 @@
+﻿namespace Comicsbox
+{
+    public class ReaderCleanerWorkerService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await DoWork(stoppingToken);
+        }
+
+        private static IEnumerable<string> GetFinalDirectories(string root)
+        {
+            List<string> result = new List<string>();
+
+            var dirs = Directory.GetDirectories(root);
+            if (!dirs.Any())
+            {
+                result.Add(root);
+            }
+            else
+            {
+                foreach(var dir in dirs)
+                {
+                    result.AddRange(GetFinalDirectories(dir));
+                }
+            }
+
+            return result;
+        }
+
+        private async Task DoWork(CancellationToken stoppingToken)
+        {
+            var tempPath = Path.Combine("wwwroot", "temp");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var dirs = await Task.Run(() => GetFinalDirectories(tempPath));
+                Console.WriteLine($"ReaderCleanerWorker: detected {dirs.Count()} directories in temp directory.");
+
+                if (stoppingToken.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                foreach (var dir in dirs)
+                {
+                    if (stoppingToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    await Task.Run(() =>
+                    {
+                        try
+                        {
+                            if (Directory.Exists(dir))
+                            {
+                                var fileInfo = new FileInfo(dir);
+                                if (fileInfo.LastWriteTimeUtc.AddHours(3) < DateTime.UtcNow)
+                                {
+                                    Directory.Delete(dir, true);
+                                    Console.WriteLine($"ReaderCleanerWorker: directory {dir} deleted.");
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        };
+                    }, stoppingToken);
+                }
+
+                if (stoppingToken.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"ReaderCleanerWorker: waiting 10min...");
+                await Task.Delay(10 * 60 * 1000, stoppingToken);
+            }
+
+            Console.WriteLine($"ReaderCleanerWorker: stopped.");
+        }
+    }
+}
